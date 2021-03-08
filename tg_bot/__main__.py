059eb402febea6116d6685e6a250087e37b309a5
zauteakm <77600757+zauteakm@users.noreply.github.com>
2021-03-08 22:51:49 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,8 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hey there! My name is *{}!*I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
+Hey there! My name is *{}!*
+I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
 
 Join my [news channel](https://t.me/DingdiSupport) to get information on all the latest updates.
 """
