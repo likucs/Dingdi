c35ba628c1fb873f17b11d6326acb62e470eefe0
zauteakm <77600757+zauteakm@users.noreply.github.com>
2021-03-14 20:59:05 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -220,8 +220,6 @@ __help__ = """
  - /adminlist: list of admins in the chat
 
 *Admin only:*
- - /pin: silently pins the message replied to - add 'loud' or 'notify' to give notifs to users.
- - /unpin: unpins the currently pinned message
  - /invitelink: gets invitelink
  - /promote: promotes the user replied to
  - /demote: demotes the user replied to
