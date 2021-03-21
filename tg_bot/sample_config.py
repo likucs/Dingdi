423bd9ee0ae70981b2bb44cea1ecc09960005e5d
TGBotSzK <77600757+ZauteKm@users.noreply.github.com>
2021-03-21 15:42:46 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,12 +10,12 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "1655319249:AAEHY2wftpk4MbJpPBOHuRdPCotLpHEH8N4"
-    OWNER_ID = "1684438752" # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "ZauteKm"
+    API_KEY = ""
+    OWNER_ID = "683538773" # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "ViruZs"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////mnt/Users/antho/Documents/sqlite_example/example.db'  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'rss']
