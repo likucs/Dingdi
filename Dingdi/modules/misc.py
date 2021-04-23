9899629604a024f0bf840e9400567a62f4a49e42
TGBotSzK <77600757+ZauteKm@users.noreply.github.com>
2021-04-23 17:03:44 +0530

    Update and rename tg_bot/modules/misc.py to Dingdi/modules/misc.py

--- Dingdi/modules/misc.py
+++ Dingdi/modules/misc.py
@@ -10,11 +10,11 @@ from telegram import ParseMode
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
-from tg_bot.__main__ import STATS, USER_INFO
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.extraction import extract_user
-from tg_bot.modules.helper_funcs.filters import CustomFilters
+from Dingdi import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
+from Dingdi.__main__ import STATS, USER_INFO
+from Dingdi.modules.disable import DisableAbleCommandHandler
+from Dingdi.modules.helper_funcs.extraction import extract_user
+from Dingdi.modules.helper_funcs.filters import CustomFilters
 
 RUN_STRINGS = (
     "Where do you think you're going?",
