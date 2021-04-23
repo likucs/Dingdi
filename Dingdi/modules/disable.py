41f3e3756815b50eda24bd3d73e7683c71c44096
TGBotSzK <77600757+ZauteKm@users.noreply.github.com>
2021-04-23 15:17:18 +0530

    Update and rename tg_bot/modules/disable.py to Dingdi/modules/disable.py

--- Dingdi/modules/disable.py
+++ Dingdi/modules/disable.py
@@ -5,9 +5,9 @@ from telegram import ParseMode, Update, Bot, Chat, User
 from telegram.ext import CommandHandler, RegexHandler, Filters
 from telegram.utils.helpers import escape_markdown
 
-from tg_bot import dispatcher
-from tg_bot.modules.helper_funcs.handlers import CMD_STARTERS
-from tg_bot.modules.helper_funcs.misc import is_module_loaded
+from Dingdi import dispatcher
+from Dingdi.modules.helper_funcs.handlers import CMD_STARTERS
+from Dingdi.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
