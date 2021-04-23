30ce8ab320296797304cc5e1de624e11ebb10573
TGBotSzK <77600757+ZauteKm@users.noreply.github.com>
2021-04-23 18:49:17 +0530

    Update and rename tg_bot/modules/sql/connection_sql.py to Dingdi/modules/sql/connection_sql.py

--- Dingdi/modules/sql/connection_sql.py
+++ Dingdi/modules/sql/connection_sql.py
@@ -3,8 +3,8 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, func, distinct
 
-from tg_bot.modules.helper_funcs.msg_types import Types
-from tg_bot.modules.sql import SESSION, BASE
+from Dingdi.modules.helper_funcs.msg_types import Types
+from Dingdi.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
