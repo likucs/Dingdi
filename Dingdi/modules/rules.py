0e70bf26091a8b3fb36afd92c20d84ab6a082825
TGBotSzK <77600757+ZauteKm@users.noreply.github.com>
2021-04-23 17:46:18 +0530

    Update and rename tg_bot/modules/rules.py to Dingdi/modules/rules.py

--- Dingdi/modules/rules.py
+++ Dingdi/modules/rules.py
@@ -6,10 +6,10 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown
 
-import tg_bot.modules.sql.rules_sql as sql
-from tg_bot import dispatcher
-from tg_bot.modules.helper_funcs.chat_status import user_admin
-from tg_bot.modules.helper_funcs.string_handling import markdown_parser
+import Dingdi.modules.sql.rules_sql as sql
+from Dingdi import dispatcher
+from Dingdi.modules.helper_funcs.chat_status import user_admin
+from Dingdi.modules.helper_funcs.string_handling import markdown_parser
 
 
 @run_async
