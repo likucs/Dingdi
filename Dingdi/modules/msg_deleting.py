07aa9f5ca596feb410145991387a1493461c5f92
TGBotSzK <77600757+ZauteKm@users.noreply.github.com>
2021-04-23 17:05:42 +0530

    Update and rename tg_bot/modules/msg_deleting.py to Dingdi/modules/msg_deleting.py

--- Dingdi/modules/msg_deleting.py
+++ Dingdi/modules/msg_deleting.py
@@ -7,9 +7,9 @@ from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import mention_html
 
-from tg_bot import dispatcher, LOGGER
-from tg_bot.modules.helper_funcs.chat_status import user_admin, can_delete
-from tg_bot.modules.log_channel import loggable
+from Dingdi import dispatcher, LOGGER
+from Dingdi.modules.helper_funcs.chat_status import user_admin, can_delete
+from Dingdi.modules.log_channel import loggable
 
 
 @run_async
