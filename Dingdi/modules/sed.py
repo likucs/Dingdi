a31f45f2173a6fd0a967f0057ed078b6b65c6780
TGBotSzK <77600757+ZauteKm@users.noreply.github.com>
2021-04-23 17:48:33 +0530

    Update and rename tg_bot/modulessed.py to Dingdi/modules/sed.py

--- Dingdi/modules/sed.py
+++ Dingdi/modules/sed.py
@@ -6,8 +6,8 @@ import telegram
 from telegram import Update, Bot
 from telegram.ext import run_async
 
-from tg_bot import dispatcher, LOGGER
-from tg_bot.modules.disable import DisableAbleRegexHandler
+from Dingdi import dispatcher, LOGGER
+from Dingdi.modules.disable import DisableAbleRegexHandler
 
 DELIMITERS = ("/", ":", "|", "_")
 
