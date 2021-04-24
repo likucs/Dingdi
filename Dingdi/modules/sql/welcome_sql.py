08b3a1c4d4b1d5590501fcb32cf42136d42e3a9d
ZauTe Km <77600757+ZauteKm@users.noreply.github.com>
2021-04-24 06:18:45 +0530

    Update welcome_sql.py

--- Dingdi/modules/sql/welcome_sql.py
+++ Dingdi/modules/sql/welcome_sql.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
 from Dingdi.modules.helper_funcs.msg_types import Types
 from Dingdi.modules.sql import SESSION, BASE
 
-DEFAULT_WELCOME = "Hey *{first},* How are you?"
+DEFAULT_WELCOME = "Hey {first}, Welcome to {chatname}. How are you?"
 DEFAULT_GOODBYE = "Nice knowing ya!"
 
 
